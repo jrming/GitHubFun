73e3f142d3254611822a07f48fea1cdc5ff25ac8
jrming <jmlai.me92g@g2.nctu.edu.tw>
2019-07-04 21:31:09 +0800

    add files

---
+++ GitHubFun/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitHubFun
+//
+//  Created by Jr-Ming Lai on 2019/7/4.
+//  Copyright © 2019 Jr-Ming Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.black
+    }
+
+
+}
+
